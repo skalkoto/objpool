4d766edd86ccfbd16366dbee4ae2028c9e1180d6
Nikos Skalkotos <skalkoto@grnet.gr>
2013-04-25 22:06:03 +0300

    Add version.py for v0.2

---
+++ objpool/version.py
@@ -0,0 +1,7 @@
+__version__ = "0.2"
+__version_info__ = ['0', '2']
+__version_vcs_info__ = {'branch': 'master',
+ 'revid': 'dca8c0f',
+ 'revno': 35,
+ 'toplevel': '/home/user/src/objpool'}
+__version_user_info__ = "user@snf-10308"
